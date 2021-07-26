1def932acbdfbccd911cd49ae3e8965d0221ea64
usov <swizze@bk.ru>
2021-07-26 14:42:15 +0300

    6

--- src/main/java/XmlCreator.java
+++ src/main/java/XmlCreator.java
@@ -2,22 +2,15 @@ import XmlStruct.Document;
 import XmlStruct.FIO;
 import XmlStruct.File;
 
-import javax.xml.bind.JAXBException;
-import java.io.FileNotFoundException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
 
 public class XmlCreator {
-    public File xmlCreator() throws JAXBException, ParseException, FileNotFoundException {
+    public File xmlCreator() {
 
 
         XmlStruct.File file = new XmlStruct.File();
 
-        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-        SimpleDateFormat formatTime = new SimpleDateFormat("HH.mm.ss");
-        Date dateNow = new Date();
+
 
         file.setIdFile("1");
         file.setVerForm("5.01");
