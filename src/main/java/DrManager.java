e4392b662333dd28bd68de13d551d0429f45648a
usov <swizze@bk.ru>
2021-07-28 16:41:13 +0300

    java-15. edited drManager

--- src/main/java/DrManager.java
+++ src/main/java/DrManager.java
@@ -1,21 +1,53 @@
+import Config.CustomSQLErrorCodeTranslator;
+import com.zaxxer.hikari.HikariConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class DrManager {
-    public void manager() {
-        String connectionUrl = "jdbc:jtds:sqlserver://SWEBAP01:1433;databaseName=REFIRA_COM;domain=REKSOFT;USENTLMV2=true";
-        String userName = "usov";
-        String pass = "S";
+
+    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
+    private SimpleJdbcCall simpleJdbcCall;
+
+    private String connectionUrl = "jdbc:jtds:sqlserver://SWEBAP01:1433;databaseName=REFIRA_COM;domain=REKSOFT;USENTLMV2=true";
+    private String userName = "usov";
+    private String pass = "S";
+
+    private HikariConfig config = new HikariConfig();
+
+
+    public String getDataSource(final DataSource dataSource){
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        final CustomSQLErrorCodeTranslator customSQLErrorCodeTranslator = new CustomSQLErrorCodeTranslator();
+        jdbcTemplate.setExceptionTranslator(customSQLErrorCodeTranslator);
+
+        namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        System.out.println(jdbcTemplate.queryForObject("SELECT COUNT(*) FROM getDocument", String.class));
+        return jdbcTemplate.queryForObject("SELECT COUNT(*) FROM getDocument", String.class);
+
+
+    }
+
+
+    public void manager()  {
+
+        config.setDriverClassName("net.sourceforge.jtds.jdbc.Driver");
         try {
             System.out.print("Connecting to SQL Server ... ");
             try (Connection connection = DriverManager.getConnection(connectionUrl, userName, pass)) {
-                System.out.println("Done.");
 
-                System.out.print("Reading data from table, press ENTER to continue...");
-                System.in.read();
-                String sql = "SELECT 'Document No_', 'Description', 'Description 2' FROM getDocument;";
+                String sql = "SELECT 'Document No_', 'Description', 'Description 2' FROM 'dbo.getDocument';";
                 try (Statement statement = connection.createStatement();
                      ResultSet resultSet = statement.executeQuery(sql)) {
                     while (resultSet.next()) {
